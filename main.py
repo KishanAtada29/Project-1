154b397ee2a777aa7758d9a3e85f69d6230bd7e9
Kishan Atada <katada@student.cscc.edu>
2026-06-15 22:07:06 -0400

    update manual function, add 6 function, and minor change in prefix function

--- main.py
+++ main.py
@@ -4,22 +4,34 @@ def class_B():
     return 16
 def class_C():
     return 8
-    
+def prefix_bits():
+    numbers = [128, 64, 32, 16, 8, 4, 2, 1]
+    return numbers
+def single_octet_bits():
+    return 8
+def number_of_octets():
+    return 4
+def total_bits():
+    return (single_octet_bits() * number_of_octets())
+def remaing_bits(n):
+    return total_bits() - n   
+def unusable_IPs():
+    return 2
 def prefix():
     subnetmasks = {
        'class A':{
            'CIDR': '/8',
-           'Number of usable IPs': (2**class_A()) -2,
+           'Number of usable IPs': (2**class_A()) - unusable_IPs(),
            'Subnetmask': '255.0.0.0'
        },
        'class B':{
            'CIDR': '/16',
-           'Number of usable IPs': (2**class_B()) - 2,
+           'Number of usable IPs': (2**class_B()) - unusable_IPs(),
            'Subnetmask': '255.255.0.0'
         },
         'class C':{
            'CIDR': '/24',
-           'Number of usable IPs': (2**class_C()) - 2,
+           'Number of usable IPs': (2**class_C()) - unusable_IPs(),
            'Subnetmask': '255.255.255.0'
         }
 
@@ -33,7 +45,19 @@ def prefix():
         print('------------------------') 
 
 def manual():
-    pass
+    cidr = int(input('Please enter CIDR (0-32): '))
+    subnetmask = ''
+    prefixList = prefix_bits()
+    usableIPs = ((2 ** remaing_bits(cidr)) - unusable_IPs())
+    print(f'CIDR: /{cidr}')
+    print(f'Number of usbale IPs: {usableIPs}')
+        
+        
+
+
+
+
+
 
 user_choice = input('Please Enter One of the Choice below\nPrefix\tManual\n')
 user_choice = user_choice.casefold()
